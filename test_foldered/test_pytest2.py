9421a49da7763ef55bf7e7b2c8cff22024783d6a
Pavel Zaikin <pzaikin@cloudlinux.com>
2019-08-08 08:18:03 +0300

    init

---
+++ test_foldered/test_pytest2.py
@@ -0,0 +1,6 @@
+def test_pytest2_test3():
+    pass
+
+
+def test_pytest2_test4():
+    assert False
---
+++ test_pytest1.py
@@ -0,0 +1,6 @@
+def test_pytest1_test1():
+    pass
+
+
+def test_pytest1_test2():
+    assert False
