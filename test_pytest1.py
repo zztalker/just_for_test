e569bdf28c395caaca5132007cb04e13514f606f
Pavel Zaikin <pzaikin@cloudlinux.com>
2019-08-14 16:32:53 +0300

    improve tests

--- test_pytest1.py
+++ test_pytest1.py
@@ -1,6 +1,21 @@
+import os
+import random
+
 def test_pytest1_test1():
     pass
 
 
 def test_pytest1_test2():
     assert False
+
+
+def test_even_failed():
+    assert int(os.environ.get("PARAM", 0)) % 2 != 0
+
+
+def test_odd_failed():
+    assert int(os.environ.get("PARAM", 0)) % 2 == 0
+
+
+def test_random():
+    assert random.randint(1, 10) >= 5
